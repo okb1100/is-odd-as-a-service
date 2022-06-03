6ab08a7d7b0664eb4745421d63f2fc2f0dab0e57
Oğuzcan Küçükbayrak <o.kucukbayrak@icloud.com>
2022-06-03 23:06:43 +0300

    fix: number type

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -10,7 +10,7 @@ export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get('/is-odd')
-  async isOddHandler(@Query('number') number: number): Promise<IsOddResponse> {
-    return { result: this.appService.getIsOdd(number) };
+  async isOddHandler(@Query('number') number: string): Promise<IsOddResponse> {
+    return { result: this.appService.getIsOdd(Number(number)) };
   }
 }
